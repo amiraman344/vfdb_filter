eb2d882b7c1b3b57a06f27a2f94580dadb20004f
amiraman344 <37573810+amiraman344@users.noreply.github.com>
2020-07-03 14:36:14 +0500

    complete VF code

--- vf.py
+++ vf.py
@@ -16,19 +16,19 @@ def extract_vf_identity(vf_data):
 
 def main(): 
     record=[]
-    vf_file_path = 'new.txt'
+    
+    vf_file_path = input("Enter Your text file:  ")
     vf_data = read_vf_file(vf_file_path)
 
     matched = extract_vf_identity(vf_data)
-    
-    
+
     for score_data in matched:
         
         score= re.finditer('Score* = *([1-9]\d*(\.\d+)?) bits* \(([^)]+)\)',score_data)
         identity=re.finditer('Identities* = [-+]?[0-9]+/[-+]?[0-9]+ \(([^)]+)\)',score_data)
         #name=re.finditer('>VFG*([0-9][0-9][0-9][0-9][0-9][0-9]) \(([^)]+)\)',score_data)
         
-        pname=list(re.finditer(r'Query= core.*\n.*\)',score_data))   
+        pname=list(re.finditer(r'Query= core.*\_Gene[0-9]\d*(\.\d+)?\n',score_data))
         
         for i,j in zip(score,identity):
            i=i.group(3)
@@ -36,16 +36,20 @@ def main():
            #k=k.group() 
            if int(i) >= 100 and int(j) >= 30 :
                record.append(pname[0].group())
-
-    with open("Filter_protein.csv","w") as f:
+    
+    with open("Filter_protein.txt","w") as f:
         for line in record:
             f.write(line)
 
-             
-            
-               
-        
-    
+    lines_seen = set() 
+    outfile = open('output.csv', "w")
+    infile = open('Filter_protein.txt', "r")
+
+    for line in infile:
+        if line not in lines_seen: 
+            outfile.write(line)
+            lines_seen.add(line)
+    outfile.close()
 
     
 if __name__ == '__main__': 
